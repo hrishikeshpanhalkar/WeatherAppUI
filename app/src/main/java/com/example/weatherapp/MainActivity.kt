6da01e64415c9cf4323ee5cb2202711226b6ab91
hrishikeshpanhalkar <hrishikeshpanhalkar7@gmail.com>
2021-12-25 22:49:34 +0530

    Added code

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,173 @@
+package com.example.weatherapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.weatherapp.ui.theme.DarkBlue
+import com.example.weatherapp.ui.theme.LightGray
+import com.example.weatherapp.ui.theme.VeryLightGray
+import com.example.weatherapp.ui.theme.WeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    WeatherPage()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WeatherPage() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 64.dp, start = 16.dp, end = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        HeaderImage()
+        MainInfo()
+        InfoTable()
+    }
+}
+
+@Composable
+fun MainInfo(){
+    Column(
+        modifier = Modifier.padding(24.dp)
+    ) {
+        Text(
+            text = "11°",
+            color = DarkBlue,
+            fontSize = 48.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "New York City, NY",
+            color = DarkBlue,
+            fontWeight = FontWeight.Medium,
+            fontSize = 20.sp,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+        Text(
+            text = "Rainy to partly cloudy. \nWinds WSW at 10 to 15km/h",
+            color = Color.Gray,
+            fontSize = 16.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(vertical = 24.dp)
+        )
+    }
+}
+
+@Composable
+fun InfoTable(){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(4.dp))
+            .background(LightGray)
+    ) {
+        Row(
+            Modifier.padding(16.dp)
+        ){
+            InfoItem(
+                iconRes = R.drawable.ic_humidity,
+                title = "Humidity",
+                subTitle = "85%",
+                modifier = Modifier.weight(
+                    1f
+                )
+            )
+            InfoItem(
+                iconRes = R.drawable.ic_sun_full,
+                title = "UV Index",
+                subTitle = "2 of 10",
+                modifier = Modifier.weight(
+                    1f
+                )
+            )
+        }
+        Divider(color = VeryLightGray)
+        Row(
+            Modifier.padding(16.dp)
+        ){
+            InfoItem(
+                iconRes = R.drawable.ic_sun_half,
+                title = "Sunrise",
+                subTitle = "7.30 AM",
+                modifier = Modifier.weight(
+                    1f
+                )
+            )
+            InfoItem(
+                iconRes = R.drawable.ic_sun_half,
+                title = "Sunset",
+                subTitle = "4.28 PM",
+                modifier = Modifier.weight(
+                    1f
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun InfoItem(iconRes: Int, title: String, subTitle: String, modifier: Modifier){
+    Row(
+        modifier = modifier
+    ){
+        Image(
+            painter = painterResource(id = iconRes),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(end = 8.dp)
+                .width(40.dp)
+        )
+        Column {
+            Text(title, color = Color.Gray)
+            Text(subTitle, color = DarkBlue, fontWeight = FontWeight.Bold)
+        }
+    }
+}
+
+@Composable
+fun HeaderImage(){
+    Image(
+        painter = painterResource(id = R.drawable.ic_couple),
+        contentDescription = null,
+        modifier = Modifier
+            .width(200.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WeatherAppTheme {
+        WeatherPage()
+    }
+}
